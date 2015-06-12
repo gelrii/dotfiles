7633eb827317ccda14e6c0b63ca50ba5705f27c6
gelrii <jsmorgan1993@gmail.com>
2015-06-13 00:06:44 +0100

    Update dotfiles.sh

--- dotfiles.sh
+++ dotfiles.sh
@@ -4,7 +4,7 @@ echo "Installing dependancies..."
 sudo dnf install recordmydesktop livestreamer rtmpdump zenity mpv aria2 youtube_dl phoronix-test-suite
 echo "Complete!"
 echo "Grabbing dotfiles..."
-git clone https://github.com/gelrii/dotfiles.git ~/.dotscripts/
+git clone https://github.com/gelrii/dotfiles/.dotscripts ~/.dotscripts/
 curl -o /home/`whoami`/.bashrc https://raw.githubusercontent.com/gelrii/dotfiles/master/.bashrc
 curl -o /home/`whoami`/.bash_aliases https://raw.githubusercontent.com/gelrii/dotfiles/master/.bash_aliases
 echo "Complete!"
